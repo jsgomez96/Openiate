e63b5ac689ab14529ada33d86e4dd6f02017efc1
Jesus Sanchez <jsgomez@tecon.es>
2025-05-15 20:57:08 +0200

    a

--- backend/settings.py
+++ backend/settings.py
@@ -41,7 +41,7 @@ class _UiSettings(BaseSettings):
         env_ignore_empty=True
     )
 
-    title: str = "Azure"
+    title: str = "Azre"
     logo: Optional[str] = None
     chat_logo: Optional[str] = None
     chat_title: str = "Empieza"
